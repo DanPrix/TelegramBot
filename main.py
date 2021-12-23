65fab18b6d14d3fc8765f07e4e920a6b7f018a56
DanPrix <74776300+DanPrix@users.noreply.github.com>
2021-12-23 10:15:54 +0300

    change main

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def start_message(message):
 
 
 @bot.message_handler(commands=['help'])
-def start_message(message):
+def help_message(message):
     bot.send_message(message.chat.id,
                      'Основные команды:\n/start - Классичекое начало пользование.\n/help - Просто помощь.\nПрочий '
                      'функционал:\nКубик{0} - Бросает шестигранную кость.\nШар судьбы{1} - Ответит на все твои вопросы,'
